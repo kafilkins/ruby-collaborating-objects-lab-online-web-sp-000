7abca81e6d90ab6dfd9f1c246c071c1862a4e097
Keith Filkins <kafilkins@me.com>
2021-01-17 22:55:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 require 'pry'
-=======
->>>>>>> 8d4fc6a831f08f051c69ca62465cf06c73b25cb7
+
 class Song
   attr_accessor :name, :artist
 
@@ -9,10 +8,9 @@ class Song
 
   def initialize(name)
     @name = name
-<<<<<<< HEAD
+
     @artist = artist
-=======
->>>>>>> 8d4fc6a831f08f051c69ca62465cf06c73b25cb7
+
     @@all << self
   end
 
@@ -20,15 +18,15 @@ class Song
     @@all
   end
 
-<<<<<<< HEAD
+
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1]
     artist = filename.split(" - ")[0]
-=======
+
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
->>>>>>> 8d4fc6a831f08f051c69ca62465cf06c73b25cb7
+
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
