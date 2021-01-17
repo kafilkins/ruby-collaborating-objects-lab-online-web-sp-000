f2823e3ebdfa113c5af1f010e1de6165b7551bc0
Keith Filkins <kafilkins@me.com>
2021-01-17 22:54:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
 
   def initialize(name)
     @name = name
-<<<<<<< HEAD
+
     @songs = [ ]
     save
   end
@@ -16,14 +16,14 @@ class Artist
 
   def save
     @@all << self
-=======
+
     @song = [ ]
     @@all << self
   end
 
   def add_song(song)
     song.artist = self
->>>>>>> 8d4fc6a831f08f051c69ca62465cf06c73b25cb7
+
   end
 
   def self.all
@@ -36,10 +36,9 @@ class Artist
       found_artist
     else
       new_artist = self.new(artist_name)
-<<<<<<< HEAD
-=======
+
       new_artist.save
->>>>>>> 8d4fc6a831f08f051c69ca62465cf06c73b25cb7
+
       new_artist
     end
   end
